00b4885f8c0310766ac9f4fa55c2294713e8a2ac
Vaibhav Tandon <vasu.tan2017@gmail.com>
2025-04-21 10:37:51 +0530

    01_basics

--- 01_basics/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-const hundreds = 100000000;
-console.log(hundreds.toExponential()); // 1e+8
-console.log(hundreds.toLocaleString("en-IN")); // 10,00,00,000
-console.log(hundreds.toLocaleString("en-US")); // 100,000,000
-console.log(hundreds.toLocaleString("en-IN", { style: 'currency', currency: 'INR' })); // ₹1,00,00,000.00
-console.log(hundreds.toLocaleString("en-US", { style: 'currency', currency: 'USD' })); // $100,000,000.00
-console.log(hundreds.toLocaleString("en-IN", { style: 'currency', currency: 'INR', minimumFractionDigits: 2 })); // ₹1,00,00,000.00
